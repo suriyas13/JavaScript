db2072668e6016607075d05ab62d41e2167d1e3f
suriyas13 <suriyak1328@gmail.com>
2024-09-08 13:15:46 +0530

    JavaScript

---
+++ Functions/functionExample.js
@@ -0,0 +1,10 @@
+var factor = "kamal"
+var fplayer = "dhoni"
+function favourite() {
+    var factor = "vijay"
+    console.log("Favourite actor:" + factor)
+    console.log("Favourite player:" + fplayer)
+}
+
+favourite();
+console.log(factor)
